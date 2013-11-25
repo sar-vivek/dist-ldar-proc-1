15108c22dd05e88d9d8447ab9851304277ddb2de
Vivek Sardeshmukh <vsardeshmukh@r-lnx213.cs.uiowa.edu>
2013-11-24 19:38:43 -0600

    triangle

--- Triangle.c
+++ Triangle.c
@@ -15,19 +15,19 @@ INT NumTri[NUM_CELLS];
 
 
 
-INT triLoc(int cellnum, LidarPointNode_t *point){
+INT triLoc(int cell, LidarPointNode_t *point){
     double xp,yp;
     INT t;
     int i,found;
     LidarPointNode_t *v1, *v2;
-    t=NumTri[cellnum];
+    t=NumTri[cell];
     found=0;
     while(!found){
         for(i=0; i<3; i++){
-            v1=TriVertex[cellnum][t][i];
-            v2=TriVertex[cellnum][t][(i+1)%3];
+            v1=TriVertex[cell][t][i];
+            v2=TriVertex[cell][t][(i+1)%3];
             if((v1->Y_c-yp)*(v2->X_c - xp) > (v1->X_c -xp)*(v2->Y_c - yp)){
-                t=TriEdge[cellnum][t][i];
+                t=TriEdge[cell][t][i];
                 found=0;
                 break;
             }
@@ -39,31 +39,62 @@ INT triLoc(int cellnum, LidarPointNode_t *point){
     return t;
 }
 
-INT triangulate(int cellnum){
+INT triangulate(int cell){
     double DMAX; /*pseudo-diagonal distance of a cell*/
 
-    DMAX=CellMax[cellnum]->X_c - CellMin[cellnum]->X_c;
-    DMAX=DMAX > CellMax[cellnum]->Y_c - CellMin[cellnum]->Y_c ? DMAX : CellMax[cellnum]->Y_c - CellMin[cellnum]->Y_c;
+    DMAX=CellMax[cell]->X_c - CellMin[cell]->X_c;
+    DMAX=DMAX > CellMax[cell]->Y_c - CellMin[cell]->Y_c ? DMAX : CellMax[cell]->Y_c - CellMin[cell]->Y_c;
 
-    return Delaunay(cellnum);
+    return Delaunay(cell);
 }
 
-INT Delaunay(int cellnum){
+INT Delaunay(int cell){
     INT numt,nt;
-    int i,j,k;
-    numt=2*CellCnt[NUM_CELLS] + 1;
+    INT i,j,k;
+    double DMAX; /*pseudo-diagonal distance of a cell*/
+    LidarPointNode_t BigTriangle[3];
+    double xcen, ycen;
+
+    DMAX=CellMax[cell]->X_c - CellMin[cell]->X_c;
+    DMAX=DMAX > CellMax[cell]->Y_c - CellMin[cell]->Y_c ? DMAX : CellMax[cell]->Y_c - CellMin[cell]->Y_c;
+
+    numt=2*CellCnt[cell] + 1;
     for(nt=0;nt<numt;nt++){
-        TriVertex[cellnum][nt]=malloc(3*sizeof(LidarPointNode_t*));
-        if(TriVertex[cellnum][nt]==NULL){
-            perror("TriVertex[cellnum][nt]");
+        TriVertex[cell][nt]=malloc(3*sizeof(LidarPointNode_t*));
+        if(TriVertex[cell][nt]==NULL){
+            perror("TriVertex[cell][nt]");
         }
     }
     for(nt=0;nt<numt;nt++){
-        TriEdge[cellnum][nt]=malloc(3*sizeof(INT));
-        if(TriEdge[cellnum][nt] == NULL){
-            perror("TriEdge[cellnum][nt])");
+        TriEdge[cell][nt]=malloc(3*sizeof(INT));
+        if(TriEdge[cell][nt] == NULL){
+            perror("TriEdge[cell][nt])");
         }
     }
 
 
+    /*calculate pseudo-triangle*/
+    xcen=0.5*(CellMax[cell]->X_c - CellMin[cell]->X_c);
+    ycen=0.5*(CellMax[cell]->Y_c - CellMin[cell]->Y_c);
+    BigTriangle[0].X_c = xcen-0.866*DMAX;
+    BigTriangle[0].Y_c = ycen-0.5*DMAX;
+    BigTriangle[1].X_c = xcen+0.866*DMAX;
+    BigTriangle[1].Y_c = ycen-0.5*DMAX;
+    BigTriangle[2].X_c = xcen;
+    BigTriangle[2].Y_c = ycen + DMAX;
+    TriVertex[cell][0][0] = &(BigTriangle[0]);
+    TriVertex[cell][0][1] = &(BigTriangle[1]);
+    TriVertex[cell][0][2] = &(BigTriangle[2]);
+    TriEdge[cell][0][0] = -1; 
+    TriEdge[cell][0][1] = -1;
+    TriEdge[cell][0][2] = -1; 
+
+    /*insert points one by one*/
+    /*for optimization we insert from bins in a specific order - details in the paper*/
+    for(nt=
+
+
+
+
+    
 
--- Triangle.h
+++ Triangle.h
@@ -12,5 +12,6 @@ extern INT NumTri[NUM_CELLS]; /*number of triangles created so far for a cell*/
 /*return triangle number of the triangle which contains pt point*/
 INT triLoc(int cellnum, LidarPointNode_t *point); 
 
+
 #endif /* ifndef TRIANGLE_H */
 
