f770388cef10349da3dfd9d1ecca0b7aece3d81a
Vivek Sardeshmukh <vsardeshmukh@r-lnx213.cs.uiowa.edu>
2013-11-25 23:53:39 -0600

    triangle - almost complete

--- Triangle.c
+++ Triangle.c
@@ -189,7 +189,8 @@ int edg(int cell, INT ix, INT nt){
 RETURNTYPE processBin(int cell, INT ix, INT iy){
     LidarPointNode_t *p, *v1, *v2, *v3;
     double px, py;
-    INT t,a,b,c;
+    INT t,a,b,c,l,r;
+    int erl,era,erb;
     p=BinTbl[cell][ix][iy];
     if(p==NULL)
         return;
@@ -233,27 +234,97 @@ RETURNTYPE processBin(int cell, INT ix, INT iy){
             TriEdge[cell][c][edg(cell,c,t)]=NumTri[cell];
             push(cell, NumTri[cell]);
         }
-        while(topstk[cell] > 0)
+        while(topstk[cell] > 0){
+            l=pop(cell);
+            r=TriEdge[cell][l][1];
+            /*circumcircle test*/
+            erl=edg(r,l);
+            era=(erl+1)%3;
+            erb=(era+1)%3;
+            v1=TriVertex[cell][r][erl];
+            v2=TriVertex[cell][r][era];
+            v3=TriVertex[cell][r][erb];
+            if(swap(cell, v1,v2,v3,p)){
+                /*p is in circle of triangle r*/
+                a=TriEdge[cell][r][era];
+                b=TriEdge[cell][r][erb];
+                c=TriEdge[cell][l][2];
+                TriVertex[cell][l][2]=v3;
+                TriEdge[cell][l][1]=a;
+                TriEdge[cell][l][2]=r;
+                TriVertex[cell][r][0]=p;
+                TriVertex[cell][r][1]=v3;
+                TriVertex[cell][r][2]=v1;
+                TriEdge[cell][r][0]=l;
+                TriEdge[cell][r][1]=b;
+                TriEdge[cell][r][2]=c;
 
-        
-
-
-
-
-        
-
-
-
-    
-    
-    
+                if(a!=BOUNDRY){
+                    TriEdge[cell][a][edg(cell,a,r)]=l;
+                    push(cell,l);
+                }
+                if(b!=BOUNDRY){
+                    push(cell,r);
+                }
+                if(c!=BOUNDRY){
+                    TriEdge[cell][c][edg(cell,c,l)]=r;
+                }
+            }
+        }
         p=p->next;
     }
-
-
 }
 
+/*
+ * circumcircle test - triangle v1 v2 v3. point p
+ * returns 0 if swap is not required
+ * else returns 1
+ */
+int swap(int cell, LidarPointNode_t *v1, LidarPointNode_t *v2, LidarPointNode_t *v3, LidarPointNode_t *p){
+    double x13, y13, x23, y23, x1p, y1p, x2p, y2p, cosa, cosb,sina,sinb;
+    x13=v1->X_c - v3->X_c;
+    y13=v1->Y_c - v3->Y_c;
+    x23=v2->X_c - v3->X_c;
+    y23=v2->Y_c - v3->Y_c;
+    x1p=v1->X_c - p->X_c;
+    y1p=v1->Y_c - p->Y_c;
+    x2p=v2->X_c - p->X_c;
+    y2p=v2->Y_c - p->Y_c;
+    cosa=x13*x23 + y13*y23;
+    cosb=x2p*x1p + y1p*y2p;
+    if((cosa > 0) && (cosb > 0)) 
+        return 0;
+    else if((cosa < 0) && (cosb < 0))
+        return 1;
+    else{
+        sina=x13*y13-x23*y13;
+        sinb=x2p*y1p-x1p*y2p;
+        if(sina*cosb+sinb*cosa < 0)
+            return 1;
+        else
+            return 0
+    }
+}
 
+void push(int cell, INT e){
+    topstk[cell]++;
+    if(topstk[cell] > CellCnt[cell]){
+        printf("%s:%d:stack full\n",__FILE__, __LINE__);
+        return;
+    }
+    estack[cell][topstk[cell]]=e;
+    return;
+}
 
+INT pop(int cell){
+    if(topstk[cell] >= 0){
+        topstk[cell]--;
+        return estack[cell][topstk[cell]+1];
+    }
+    else{
+        printf("%s:%d:stack empty\n",__FILE__, __LINE__);
+        exit(-1);
+    }
+}
 
 
