ddad760cc1d64f955ef7d960e9687d5c0c93081d
James Wesley Hegeman <jwhegeman@r-lnx228.cs.uiowa.edu>
2013-11-25 21:55:54 -0600

    new files

---
+++ MergeMaster.h
@@ -0,0 +1,14 @@
+/*JWHGPLHERE*/
+/*
+ * MergeMaster.h
+ *
+ * Author: James W Hegeman
+ *
+ */
+
+#ifndef __MERGE_MASTER_H__
+#define __MERGE_MASTER_H__
+
+void MergeReceive();
+
+#endif
---
+++ MergeSlave.h
@@ -0,0 +1,14 @@
+/*JWHGPLHERE*/
+/*
+ * MergeSlave.h
+ *
+ * Author: James W Hegeman
+ *
+ */
+
+#ifndef __MERGE_SLAVE_H__
+#define __MERGE_SLAVE_H__
+
+void MergeSend();
+
+#endif
