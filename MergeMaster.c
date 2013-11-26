b88d3fae0cc50a3cdf16151d0fdf762c0f465496
James Wesley Hegeman <jwhegeman@r-lnx228.cs.uiowa.edu>
2013-11-25 21:50:57 -0600

    new files

---
+++ MergeMaster.c
@@ -0,0 +1,28 @@
+/*JWHGPLHERE*/
+/*
+ * MergeMaster.c
+ *
+ * Author: James W Hegeman
+ *
+ */
+
+#include "DLPstd.h"
+#include "DistLdarProcFVar.h"
+#include "MergeMaster.h"
+
+void MergeReceive() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ MergeSlave.c
@@ -0,0 +1,22 @@
+/*JWHGPLHERE*/
+/*
+ * MergeSlave.c
+ *
+ * Author: James W Hegeman
+ *
+ */
+
+#include "DLPstd.h"
+#include "DistLdarProcFVar.h"
+#include "MergeSlave.h"
+
+void MergeSend() {
+
+
+
+
+
+
+
+
+}
