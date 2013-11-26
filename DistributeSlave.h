d2b38325d28ca4aea8530e4a6a10312db8c05bd8
James Wesley Hegeman <jwhegeman@r-lnx228.cs.uiowa.edu>
2013-11-25 21:59:33 -0600

    new files

---
+++ DistributeSlave.h
@@ -0,0 +1,14 @@
+/*JWHGPLHERE*/
+/*
+ * DistributeSlave.h
+ *
+ * Author: James W Hegeman
+ *
+ */
+
+#ifndef __DISTRIBUTE_SLAVE_H__
+#define __DISTRIBUTE_SLAVE_H__
+
+void DistributeReceive();
+
+#endif
