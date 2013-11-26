fed81dee48fb8a279bbe41c825c4c32a2f6cda89
James Wesley Hegeman <jwhegeman@r-lnx228.cs.uiowa.edu>
2013-11-25 22:55:24 -0600

    new files

---
+++ ProcessData.c
@@ -0,0 +1,18 @@
+/*JWHGPLHERE*/
+/*
+ * ProcessData.c
+ *
+ * Author: James W Hegeman
+ *
+ */
+
+#include "DLPstd.h"
+#include "ProcessData.h"
+
+void *ProcessData(void *workerID) {
+
+
+
+
+
+}
---
+++ ProcessData.h
@@ -0,0 +1,14 @@
+/*JWHGPLHERE*/
+/*
+ * ProcessData.h
+ *
+ * Author: James W Hegeman
+ *
+ */
+
+#ifndef __PROCESS_DATA_H__
+#define __PROCESS_DATA_H__
+
+void *ProcessData(void *workerID);
+
+#endif
