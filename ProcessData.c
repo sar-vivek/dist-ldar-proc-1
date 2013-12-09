eda27b964adae19b92e24715b9a42430b277ca0a
James Wesley Hegeman <jwhegeman@r-lnx228.cs.uiowa.edu>
2013-12-08 22:53:30 -0600

    update

--- ProcessData.c
+++ ProcessData.c
@@ -144,7 +144,7 @@ void *ProcessData(void *workerID) {
     for (ix = 0; ix < NumPointRec; ++ix) {
 	if (*(FiltTbl + ix) == 0) ++count2;
     }
-    printf("There are %u points remaining after filtering.\n", count2);
+    printf("There are %u (original) points remaining after filtering.\n", count2);
     fflush(stdout);
 #endif
 
