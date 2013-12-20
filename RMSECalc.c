f69e06e0ee2b52c4b8090de9d28cc23ad4224bbc
James Wesley Hegeman <jwhegeman@r-lnx228.cs.uiowa.edu>
2013-12-20 15:34:48 -0600

    Verifier files

---
+++ RMSECalc.c
@@ -0,0 +1,37 @@
+/*JWHGPLHERE*/
+/*
+ * RMSECalc.c
+ *
+ * Author: James W Hegeman
+ *
+ */
+
+#include "DLPstd.h"
+#include "LdarReader.h"
+#include "RMSECalc.h"
+
+int RMSECalc(int argc, char *argv[]) {
+
+    FILE *tri_file_in;
+
+
+
+
+
+    LasFileInit(argv[1]);
+
+
+
+
+
+    return 0;
+}
+
+double FindZ(LidarPointNode_t *A, LidarPointNode_t *B, LidarPointNode_t *C) {
+
+
+
+
+
+
+}
---
+++ RMSECalc.h
@@ -0,0 +1,14 @@
+/*JWHGPLHERE*/
+/*
+ * RMSECalc.h
+ *
+ * Author: James W Hegeman
+ *
+ */
+
+#ifndef __RMSE_CALC_H__
+#define __RMSE_CALC_H__
+
+double FindZ(LidarPointNode_t *A, LidarPointNode_t *B, LidarPointNode_t *C);
+
+#endif
