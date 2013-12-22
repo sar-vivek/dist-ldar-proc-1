4fe36f463c90c605011b93fe14d28a16ac08cf4f
James W Hegeman <james-hegeman@uiowa.edu>
2013-12-22 13:49:03 -0600

    update

--- DLPstd.h
+++ DLPstd.h
@@ -25,14 +25,14 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define DEBUG 3
+#define DEBUG 4
 /*interpret double zero to be in between -DZERO to +DZERO*/
 #define DZERO 1.0e-8 
 #define NODE_POINTS_MAX 16777216
 
 /* NUM_BINS_X and NUM_BINS_Y should be even */
-#define NUM_BINS_X 4
-#define NUM_BINS_Y 4
+#define NUM_BINS_X 2
+#define NUM_BINS_Y 2
 
 #define NUM_CELLS 2
 #define NUM_CELLS_X 2
