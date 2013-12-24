efb6d53cbc53df34d17cae0cfab5df7b4e8b5252
James W Hegeman <james-hegeman@uiowa.edu>
2013-12-23 18:17:05 -0600

    updates

--- RMSECalc.h
+++ RMSECalc.h
@@ -9,6 +9,6 @@
 #ifndef __RMSE_CALC_H__
 #define __RMSE_CALC_H__
 
-double FindZ(LidarPointNode_t *A, LidarPointNode_t *B, LidarPointNode_t *C);
+double FindZ(LidarPointNode_t *A, LidarPointNode_t *B, LidarPointNode_t *C, LidarPointNode_t *X);
 
 #endif
